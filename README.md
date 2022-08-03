ba25f2e56dfb92ee71fb540e5d4d060cc74ad902
stefanoyoyo <git.stefano@outlook.it>
2022-08-03 12:50:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-k6x7e5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-k6x7e5)
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+      <kendo-chart>
+        <kendo-chart-series>
+          <kendo-chart-series-item type="bar" [stack]="false" [data]="[1, 2, 3]">
+          </kendo-chart-series-item>
+          <kendo-chart-series-item type="bar" [data]="[1, -1, 1]">
+          </kendo-chart-series-item>
+          <kendo-chart-series-item type="bar" [data]="[1, -1, 1]">
+          </kendo-chart-series-item>
+        </kendo-chart-series>
+      </kendo-chart>
+    `
+})
+export class AppComponent {
+}
